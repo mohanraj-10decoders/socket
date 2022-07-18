852c076277b4c55d6ef7439f52e14514025e0341
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-07-18 16:34:20 +0530

    BE commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+
+const server = require('http').createServer(app);
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('Socket is active to connect');
+
+  socket.on('chat', (payload) => {
+    console.log('Payload for chat', payload);
+    io.emit('chat', payload);
+  });
+});
+
+server.listen('5000', () => {
+  console.log('Server is listening at port 5000');
+});
